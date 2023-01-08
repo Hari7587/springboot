88dc70bc7f77a5811151634deba280c2f928035d
Hari7587 <95676845+Hari7587@users.noreply.github.com>
2023-01-08 11:31:20 +0530

    Update DockerHelloWorld.java

--- src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
+++ src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
@@ -11,6 +11,6 @@ public class DockerHelloWorld {
 	@GetMapping
 	public String getName() {
 		
-		return "Welcome to BitstreamIO training using Git Maven Docker Jenkins....!";
+		return "Welcome to the BitstreamIO training using Git Maven Docker Jenkins....!";
 	}
 }
